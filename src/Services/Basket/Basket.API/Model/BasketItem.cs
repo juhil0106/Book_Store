c138bff5b3132e0232f5e7bd510527216d937d2b
DipKavaiya <dippanchal02@gmail.com>
2023-11-02 12:07:51 +0530

    Implemented APIs that manage cart item and cart details.

---
+++ src/Services/Basket/Basket.API/Model/BasketItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Basket.API.Model
+{
+    public class BasketItem
+    {
+        public string BookId { get; set; } = null!;
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
